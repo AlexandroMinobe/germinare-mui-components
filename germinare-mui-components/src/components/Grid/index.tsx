40a26a2ca4731dde08188d9d8102e9301136fb14
AlexandroMinobe <alexkatsuharu@gmail.com>
2021-12-16 11:47:46 -0300

    componentização grid

---
+++ germinare-mui-components/src/components/Grid/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+import { Grid } from '@mui/material'
+
+export interface IGridComponentProp {
+  value?: string
+  children?: any
+  container?: boolean
+  item?: boolean
+  direction: 	'column-reverse' | 'column' | 'row-reverse' | 'row'
+  xs: 'auto' | number | boolean
+  sm: 'auto' | number | boolean
+  md: 'auto' | number | boolean
+  lg: 'auto' | number | boolean
+  xl: 'auto' | number | boolean
+
+}
+
+export default function GridComponent(
+  props: IGridComponentProp
+) {
+  return (
+    <Grid
+      direction={props.direction}
+      item={props.item}
+      container={props.container}
+      spacing={2}
+      xs={props.xs}
+      sm={props.sm}
+      md={props.md}
+      lg={props.lg}
+      xl={props.xl}
+    >
+    </Grid>
+  )
+}
